c374fde3bd147328f621ac19abecb1b9feb790af
MaxBeaulieu <maxbeaulieu@live.ca>
2018-02-19 18:50:53 -0500

    Initial load

---
+++ Caster.py
@@ -0,0 +1,11 @@
+class Caster:
+    def __init__(self, first_name, last_name, handle_name, email, twitter, facebook, instagram):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.handle_name = handle_name
+        self.email = email
+        self.twitter = twitter
+        self.facebook = facebook
+        self.instagram = instagram
+
+    
---
+++ RocketLeague.py
@@ -0,0 +1,7 @@
+class RocketLeague:
+    def __init__(self, blue_team, blue_score, orange_team, orange_score, boX):
+        self.blue_team = blue_team
+        self.blue_score = blue_score
+        self.orange_team = orange_team
+        self.orange_score = orange_score
+        self.boX = boX
